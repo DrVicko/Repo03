2965cdfdc71501536d0111519be7e44da8040469
Vedran V <drvicko@hotmail.com>
2022-03-30 22:15:14 +0200

    JS comments

---
+++ ex02/src/index.js
@@ -0,0 +1,17 @@
+function main () 
+{
+    // One two Three Four Five Six Seven etc...
+
+    /*
+    One
+    Two
+    Three
+    Four
+    Five
+    Six
+    Seven
+    etc...
+    */
+}
+main();
+module.exports = main; 
