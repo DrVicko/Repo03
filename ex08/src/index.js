82586d44a28f4db99b41e9ed3d323fd5fb6176ff
Vedran V <drvicko@hotmail.com>
2022-03-30 22:53:25 +0200

    Sum of two variables

---
+++ ex08/src/index.js
@@ -0,0 +1,12 @@
+function main (numOne, numTwo) 
+{
+    var sum;
+    var numOne;
+    var numTwo; 
+
+    sum = numOne + numTwo; 
+   
+    return sum;
+}
+console.log(main(15, 8));
+module.exports = main;
