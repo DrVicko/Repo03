b859b92a1ef7defd3eeb4623a532fe68b0a074ff
Vedran V <drvicko@hotmail.com>
2022-03-30 23:14:31 +0200

    Multiply two variables

---
+++ ex10/src/index.js
@@ -0,0 +1,14 @@
+function main (numOne, numTwo) 
+{
+    // Only change code below this line
+    var product;
+    var numOne;
+    var numTwo; 
+
+    product = numOne * numTwo; 
+    // Only change code above this line
+   
+    return product;
+}
+console.log(main(15, 8));
+module.exports = main;
